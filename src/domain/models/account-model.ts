0a3ebeeccc5d2d2b437821c37021935fec248e05
Mateus Araujo <mateusdearaujo@hotmail.com.br>
2022-04-16 17:19:06 -0300

    feat: add domain contracts

---
+++ src/domain/models/account-model.ts
@@ -0,0 +1,5 @@
+export type AccountModel = {
+  id: number
+  username: string
+  password: string
+}
---
+++ src/domain/usecases/index.ts
@@ -0,0 +1,2 @@
+export * from '~/domain/usecases/add-account'
+export * from '~/domain/usecases/authentication'
